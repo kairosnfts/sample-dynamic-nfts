cfd81482ee72102230dc7720c1f3b96af77abe0c
Michael Mercer <michael.mercer@hey.com>
2023-04-19 16:27:17 -0500

    Metadata wasn't being set on create

--- app/api/nft/helpers.ts
+++ app/api/nft/helpers.ts
@@ -47,6 +47,66 @@ export const getNextStage = (nft: Nft) => {
   return nextStage
 }
 
+type UpdateNftProps = {
+  nftId: string
+  image: string
+  stage: TreeStage
+  description: string
+}
+
+export const updateNft = async ({
+  nftId,
+  image,
+  stage,
+  description,
+}: UpdateNftProps) => {
+  const date = new Date()
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear()
+  const hour = date.getHours().toString().padStart(2, '0')
+  const minute = date.getMinutes().toString().padStart(2, '0')
+  const second = date.getSeconds().toString().padStart(2, '0')
+  const niceDate = `${month}/${day}/${year} ${hour}:${minute}:${second}`
+
+  return await request(
+    process.env.NEXT_PUBLIC_KAIROS_API_URL!,
+    UpdateMetadataQuery,
+    {
+      input: {
+        nftId: nftId,
+        /**
+         * Metadata patch is a JSON object that will be merged with the existing
+         * metadata. You can use this to update the metadata of an NFT without
+         * having to redeploy it. This is useful for updating the image of an
+         * NFT, or adding new attributes
+         */
+        metadataPatch: {
+          image: image,
+          attributes: [
+            // You can add as many attributes as you want
+            {
+              trait_type: 'Bonsai Stage',
+              value: stage,
+            },
+            {
+              trait_type: 'Description',
+              value: description,
+            },
+            {
+              trait_type: 'Last Cultivated On',
+              value: niceDate,
+            },
+          ],
+        },
+      },
+    },
+    {
+      Authorization: auth,
+    }
+  )
+}
+
 /**
  * We use GraphQL queries to interact with the Kairos API
  * You can learn more about GraphQL language here: https://graphql.org/learn/
